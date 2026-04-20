4977b6c74d9ea7bb17da2492c88d2cc4e57e654d
jules2pommes <jules.jag.28@gmail.com>
2026-04-20 14:40:08 +0200

    setup

---
+++ src/ChiffresRomains.java
@@ -0,0 +1,13 @@
+
+public class ChiffresRomains {
+
+	public ChiffresRomains() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String toChiffresRomains(int nombre) {
+		StringBuilder sb = new StringBuilder();
+		return sb.toString();
+	}
+
+}
